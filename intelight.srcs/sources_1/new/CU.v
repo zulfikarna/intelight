f45331d3260f55a55e7a3dca91ff668d89228c43
zulfikarna <z.arifuzzaki@gmail.com>
2022-02-21 13:40:37 +0700

    Pembuatan CU

---
+++ intelight.srcs/sources_1/new/CU.v
@@ -0,0 +1,72 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// CONTROL UNIT
+// Engineer : 13218029 Zulfikar
+//////////////////////////////////////////////////////////////////////////////////
+
+module CU(
+    input wire clk, rst, start,
+    
+    // From Processing System 
+    input wire [15:0] max_step,
+    input wire [15:0] max_episode,
+    
+    // From Programmable Logic 
+    input wire curr_state,
+    input wire act,
+    input wire act_sel,
+    
+    // Control Signal
+    output wire PG,
+    output wire QA,
+    output wire SD,
+    output wire RD,
+    output reg finish,
+    );
+    
+    // State variable for FSM implementation 
+    localparam
+        S_IDLE  = 8'hFF,
+        S_INIT  = 8'HEE;
+        
+    // State transition variable
+    reg [7:0] cs;
+    reg [7:0] ns;
+    
+    // Counter variabel 
+    reg [15:0] cs;
+    reg [15:0] ec;
+    reg [15:0] epsilon;
+    
+    // Step and Episode Counter Machine 
+    always@(posedge clk) begin
+        // Step Counter
+        if((cs == S_L0)) begin
+            sc = sc + 1;
+        end else if (csw == S_INIT) begin
+            sc = 16'd0;
+        end else begin
+            sc = sc;
+        end
+        // Episode Counter 
+        if(cs == S_IDLE) begin
+            ec = 16'd0;
+        end else if (cs == S_LX) begin
+            ec = ec + 16'd1;
+        end else begin
+            ec = ec;
+        end
+    end
+    
+    // Epsilon updating 
+    always @(ec) begin
+        if (cs == S_IDLE) begin
+            epsilon = 11'd0;
+        end else begin
+            epsilon = max_episode - ec;
+        end
+    end
+    
+    // Control signals go here :
+    
+endmodule
